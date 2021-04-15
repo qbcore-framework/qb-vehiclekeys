3d66bafbf98ef2479daa6560b2f530a8590a695d
LordLucifr <67039969+LordLucifr@users.noreply.github.com>
2021-04-15 05:02:56 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,15 +1,3 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-}
-
-
 QBCore = nil
 
 local HasKey = false
@@ -70,16 +58,16 @@ Citizen.CreateThread(function()
             QBCore.Functions.DrawText3D(vehpos.x, vehpos.y, vehpos.z, "[G] Search / [H] Hotwire" )
             SetVehicleEngineOn(veh, false, false, true)
 
-            if IsControlJustPressed(0, Keys["H"]) then
+            if IsControlJustPressed(0, 74) then
                 Hotwire()
             end
 
-            if IsControlJustPressed(1, Keys["G"]) then
+            if IsControlJustPressed(1, 47) then
                 Search()
             end
         end
 
-        if IsControlJustPressed(1, Keys["L"]) then
+        if IsControlJustPressed(1, 182) then
             LockVehicle()
         end
     end
