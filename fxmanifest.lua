1ebd55e8a96aff86659a302deb434d97216012a1
buddizer <lucas@LMBP.local>
2022-05-05 00:08:55 -0600

    added calls in fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,12 @@ game 'gta5'
 description 'QB-VehicleKeys'
 version '1.0.0'
 
-shared_script 'config.lua'
+shared_script {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua'
+}
+
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
