b4978341f4ec0c3247cdb2cea47053e7c402f035
buddizer <lucasg@live.com>
2022-05-04 23:35:53 -0600

    Create en.lua

---
+++ locale/en.lua
@@ -0,0 +1,14 @@
+local Translations = {
+    error = {
+    },
+    success = {
+    },
+    info = {
+    },
+    warning = {},
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
