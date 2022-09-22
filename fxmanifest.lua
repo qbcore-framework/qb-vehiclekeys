f284a8a7927380a6a6427e70b5fdc67d148a8af4
buddizer <none>
2022-09-22 10:29:37 -0600

    auto switch locale system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,10 @@ description 'QB-VehicleKeys'
 version '1.0.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua',
-	'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
 }
 client_script 'client/main.lua'
 server_script 'server/main.lua'
