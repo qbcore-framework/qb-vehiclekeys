02c329d0feb776a76815382c64ae91625099ab8c
konarPlus <57233235+ahmed608@users.noreply.github.com>
2022-10-01 12:52:10 +0200

    make keyfob configurable option
    add keyfob and NUICallbacks

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 -- NPC Vehicle Lock States
 Config.LockNPCDrivingCars = true -- Lock state for NPC cars being driven by NPCs [true = locked, false = unlocked]
 Config.LockNPCParkedCars = true -- Lock state for NPC parked cars [true = locked, false = unlocked]
-
+Config.UseKeyfob = false -- you can set this true if you dont need ui
 -- Lockpick Settings
 Config.RemoveLockpickNormal = 0.5 -- Chance to remove lockpick on fail
 Config.RemoveLockpickAdvanced = 0.2 -- Chance to remove advanced lockpick on fail
