433b6b71d5c9ce16e15b5cb45abe55aee855943e
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-12-22 23:14:36 +0100

    Update sv.lua

--- locales/sv.lua
+++ locales/sv.lua
@@ -41,7 +41,9 @@ local Translations = {
 
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'sv' then
+    Lang = Lang or Locale:new({
+        phrases = Translations,
+        warnOnMissing = true
+    })
+end
