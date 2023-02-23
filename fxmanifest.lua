9c8566358c6f6b66df6f09c842152d0405e22394
AnishBplayz <91739770+AnishBplayz@users.noreply.github.com>
2023-02-23 00:36:34 +0000

    Fix console error : attempt to index a nil value (#195)
    * ['FIX'] Console error : attempt to index a nil value
    
    [script:qb-vehiclekey] SCRIPT ERROR: @qb-vehiclekeys/server/main.lua:53: attempt to index a nil value
    [script:qb-vehiclekey] > TriggerCallback (@qb-core/server/functions.lua:251)
    [script:qb-vehiclekey] > handler (@qb-core/server/events.lua:152)
    [      script:qb-core] SCRIPT ERROR: error object is not a string
    
    Line 53 attempt to index a nil value fixed
    
    * Bump
    
    ---------
    
    Co-authored-by: zwkf <dev@charlie.fyi>

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,23 @@
 fx_version 'cerulean'
 game 'gta5'
 description 'QB-VehicleKeys'
-version '1.2.3'
+version '1.2.4'
 ui_page 'NUI/index.html'
+
 files {
     'NUI/index.html',
     'NUI/style.css',
     'NUI/script.js',
     'NUI/images/*',
 }
+
 shared_scripts {
     '@qb-core/shared/locale.lua',
     'locales/*.lua',
     'config.lua',
 }
+
 client_script 'client/main.lua'
 server_script 'server/main.lua'
+
 lua54 'yes'
