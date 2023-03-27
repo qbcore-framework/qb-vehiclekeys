7424c2182354721669300e8d8dbd741f52a2a19c
konarPlus <57233235+ahmed608@users.noreply.github.com>
2023-03-27 12:17:06 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,11 +4,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local KeysList = {}
 local isCarjacking = false
-local canCarjack = true
 local AlertSend = false
 local lastPickedVehicle = nil
 local usingAdvanced = false
-local IsHotwiring = false
 local trunkclose = true
 local IsRobbing = false
 local lockpicked = false
@@ -22,12 +20,12 @@ CreateThread(function()
         if LocalPlayer.state.isLoggedIn then
             local ped = PlayerPedId()
             local entering = GetVehiclePedIsTryingToEnter(ped)
+            local driver = GetPedInVehicleSeat(entering, -1)
+            local plate = QBCore.Functions.GetPlate(entering)
             if entering ~= 0 then
                 sleep = 2000
-                local plate = QBCore.Functions.GetPlate(entering)
                 QBCore.Functions.TriggerCallback('vehiclekeys:CheckOwnership', function(result)
                     if not result then -- if not player owned
-                        local driver = GetPedInVehicleSeat(entering, -1)
                         if driver ~= 0 and not IsPedAPlayer(driver) then
                             if Config.Rob then
                                 if IsEntityDead(driver) then
@@ -45,7 +43,7 @@ CreateThread(function()
                         else
                             if not lockpicked and lockpickedPlate ~= plate then
                                 QBCore.Functions.TriggerCallback('vehiclekeys:CheckHasKey', function(result)
-                                    if result == false then
+                                    if not result then
                                         SetVehicleDoorsLocked(entering, 2)
                                     else
                                         HasKey = true
@@ -552,139 +550,6 @@ function LockpickDoor(isAdvanced)
     usingAdvanced = isAdvanced
     Config.LockPickDoorEvent()
 end
-function LockpickFinishCallback(success)
-    local vehicle = QBCore.Functions.GetClosestVehicle()
-
-    local chance = math.random()
-    if success then
-        TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
-        lastPickedVehicle = vehicle
-
-        if GetPedInVehicleSeat(vehicle, -1) == PlayerPedId() then
-            TriggerServerEvent('qb-vehiclekeys:server:AcquireVehicleKeys', QBCore.Functions.GetPlate(vehicle))
-        else
-            QBCore.Functions.Notify(Lang:t("notify.vlockpick"), 'success')
-            TriggerServerEvent('qb-vehiclekeys:server:setVehLockState', NetworkGetNetworkIdFromEntity(vehicle), 1)
-        end
-
-    else
-        TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
-        AttemptPoliceAlert("steal")
-    end
-
-    if usingAdvanced then
-        if chance <= Config.RemoveLockpickAdvanced then
-            TriggerServerEvent("qb-vehiclekeys:server:breakLockpick", "advancedlockpick")
-        end
-    else
-        if chance <= Config.RemoveLockpickNormal then
-            TriggerServerEvent("qb-vehiclekeys:server:breakLockpick", "lockpick")
-        end
-    end
-end
-
-function Hotwire(vehicle, plate)
-    local hotwireTime = math.random(Config.minHotwireTime, Config.maxHotwireTime)
-    local ped = PlayerPedId()
-    IsHotwiring = true
-
-    SetVehicleAlarm(vehicle, true)
-    SetVehicleAlarmTimeLeft(vehicle, hotwireTime)
-    QBCore.Functions.Progressbar("hotwire_vehicle", Lang:t("progress.hskeys"), hotwireTime, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@",
-        anim = "machinic_loop_mechandplayer",
-        flags = 16
-    }, {}, {}, function() -- Done
-        StopAnimTask(ped, "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 1.0)
-        TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
-        if (math.random() <= Config.HotwireChance) then
-            TriggerServerEvent('qb-vehiclekeys:server:AcquireVehicleKeys', plate)
-        else
-            QBCore.Functions.Notify(Lang:t("notify.fvlockpick"), "error")
-        end
-        Wait(Config.TimeBetweenHotwires)
-        IsHotwiring = false
-    end, function() -- Cancel
-        StopAnimTask(ped, "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 1.0)
-        IsHotwiring = false
-    end)
-    SetTimeout(10000, function()
-        AttemptPoliceAlert("steal")
-    end)
-    IsHotwiring = false
-end
-function CarjackVehicle(target)
-    if not Config.CarJackEnable then return end
-    isCarjacking = true
-    canCarjack = false
-    loadAnimDict('mp_am_hold_up')
-    local vehicle = GetVehiclePedIsUsing(target)
-    local occupants = GetPedsInVehicle(vehicle)
-    for p=1,#occupants do
-        local ped = occupants[p]
-        CreateThread(function()
-            TaskPlayAnim(ped, "mp_am_hold_up", "holdup_victim_20s", 8.0, -8.0, -1, 49, 0, false, false, false)
-            PlayPain(ped, 6, 0)
-        end)
-        Wait(math.random(200,500))
-    end
-    -- Cancel progress bar if: Ped dies during robbery, car gets too far away
-    CreateThread(function()
-        while isCarjacking do
-            local distance = #(GetEntityCoords(PlayerPedId()) - GetEntityCoords(target))
-            if IsPedDeadOrDying(target) or distance > 7.5 then
-                TriggerEvent("progressbar:client:cancel")
-            end
-            Wait(100)
-        end
-    end)
-    QBCore.Functions.Progressbar("rob_keys", Lang:t("progress.acjack"), Config.CarjackingTime, false, true, {}, {}, {}, {}, function()
-        local hasWeapon, weaponHash = GetCurrentPedWeapon(PlayerPedId(), true)
-        if hasWeapon and isCarjacking then
-            local carjackChance
-            if Config.CarjackChance[tostring(GetWeapontypeGroup(weaponHash))] then
-                carjackChance = Config.CarjackChance[tostring(GetWeapontypeGroup(weaponHash))]
-            else
-                carjackChance = 0.5
-            end
-            if math.random() <= carjackChance then
-                local plate = QBCore.Functions.GetPlate(vehicle)
-                    for p=1,#occupants do
-                        local ped = occupants[p]
-                        CreateThread(function()
-                        TaskLeaveVehicle(ped, vehicle, 0)
-                        PlayPain(ped, 6, 0)
-                        Wait(1250)
-                        ClearPedTasksImmediately(ped)
-                        PlayPain(ped, math.random(7,8), 0)
-                        MakePedFlee(ped)
-                    end)
-                end
-                TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
-                TriggerServerEvent('qb-vehiclekeys:server:AcquireVehicleKeys', plate)
-            else
-                QBCore.Functions.Notify(Lang:t("notify.cjackfail"), "error")
-                MakePedFlee(target)
-                TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
-            end
-            isCarjacking = false
-            Wait(2000)
-            AttemptPoliceAlert("carjack")
-            Wait(Config.DelayBetweenCarjackings)
-            canCarjack = true
-        end
-    end, function()
-        MakePedFlee(target)
-        isCarjacking = false
-        Wait(Config.DelayBetweenCarjackings)
-        canCarjack = true
-    end)
-end
 
 function AttemptPoliceAlert(type)
     if not AlertSend then
@@ -704,20 +569,6 @@ end
 function MakePedFlee(ped)
     SetPedFleeAttributes(ped, 0, 0)
     TaskReactAndFleePed(ped, PlayerPedId())
-end
-function DrawText3D(x, y, z, text)
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x, y, z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0 + 0.0125, 0.017 + factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
 end
 -----------------------
 ----   NUICallback   ----
