b0128a43c43504e8ebacaf9419895d09b0f02f99
Sigurd <81210348+SjurdVrancken@users.noreply.github.com>
2024-10-14 21:27:43 +0200

    fix (vehiclekeys): added missing language check

--- locales/nl.lua
+++ locales/nl.lua
@@ -41,7 +41,11 @@ local Translations = {
 
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'nl' then
+    Lang = Lang or Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
+
